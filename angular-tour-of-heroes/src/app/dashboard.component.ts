af6c66594bf60147d0d8ae3a7c22463aeaec1202
Jun Wang <junwang@Juns-MacBook-Pro.local>
2017-02-14 17:02:12 -0800

    router done

--- angular-tour-of-heroes/src/app/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 @Component({
+    moduleId: module.id,//WebStorm report error, but it is actually working
     selector: 'my-dashboard',
-    template: '<h3>My Dashboard</h3>'
+    templateUrl: './dashboard.component.html',
+    styleUrls: [ './dashboard.component.css' ]
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit{
+    heroes: Hero[] = [];
+    constructor(private heroService: HeroService){
+
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes=>this.heroes = heroes.slice(1, 5));
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+}
